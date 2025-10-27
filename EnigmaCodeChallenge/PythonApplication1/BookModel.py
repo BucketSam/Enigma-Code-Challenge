1d5047d53865f20c3688ff8a1778646225510f07
BucketSam <samuele.scozzai@gmail.com>
2025-10-27 12:41:06 +0100

    Added forgotted Id to book model

--- EnigmaCodeChallenge/PythonApplication1/BookModel.py
+++ EnigmaCodeChallenge/PythonApplication1/BookModel.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
 from datetime import date
+from typing import Optional
 
 class Book(BaseModel):
+    id: Optional[int] = None 
     title: str
     author: str
-    published_date: date | None = None
+    published_date: Optional[date] = None
     isbn: str
 
